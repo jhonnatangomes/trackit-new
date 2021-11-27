0e7d6110aab3d941cd71a96709fff8ef891317ba
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-11-27 15:13:42 -0300

    implemented protected routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import GlobalStyle from './styles/globalStyles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/login/Login';
-import Today from './components/login/today/Today';
+import Today from './components/today/Today';
+import Habits from './components/habits/Habits';
+import ProtectedRoute from './components/protectedRoute/ProtectedRoute';
+import { useState } from 'react';
+import UserContext from './contexts/UserContext';
 
 function App() {
+    const [user, setUser] = useState(null);
+
     return (
         <BrowserRouter>
-            <GlobalStyle />
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/cadastro" element={<Login />} />
-                <Route path="/hoje" element={<Today />} />
-            </Routes>
+            <UserContext.Provider value={{ user, setUser }}>
+                <GlobalStyle />
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/cadastro" element={<Login />} />
+                    <Route
+                        path="/hoje"
+                        element={
+                            <ProtectedRoute isAuthenticated={user}>
+                                <Today />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/habitos"
+                        element={
+                            <ProtectedRoute isAuthenticated={user}>
+                                <Habits />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Routes>
+            </UserContext.Provider>
         </BrowserRouter>
     );
 }
---
+++ src/components/habits/Habits.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export default function Habits() {
+    return <span>Hábitos</span>;
+}
